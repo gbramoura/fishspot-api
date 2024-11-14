276c52f8083c991ad70496ed08ec93dac25088e8
Gabriel Moura <gabrielalves.dev@gmail.com>
2024-11-14 18:37:37 -0300

    add mongodb context to the project

---
+++ src/FishspotApi.Core/CoreDependency.cs
@@ -0,0 +1,17 @@
+﻿using FishspotApi.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FishspotApi.Core
+{
+    public static class CoreDependency
+    {
+        /// <summary>
+        /// Method to include the services of core lib into depency injection
+        /// </summary>
+        /// <param name="services">Extesion of IServiceCollection</param>
+        public static void AddCoreDependency(this IServiceCollection services)
+        {
+            services.AddDataDependency();
+        }
+    }
+}
---
+++ src/FishspotApi.Data/Context/FishspotApiContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishspotApi.Data.Context
+{
+    public class FishspotApiContext
+    {
+        private readonly IMongoDatabase? _database;
+
+        public FishspotApiContext(IConfiguration config)
+        {
+            var dbConnection = config["MongoDB:ConnectionURI"];
+            var mongoUrl = MongoUrl.Create(dbConnection);
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        public IMongoDatabase? Database => _database;
+    }
+}
