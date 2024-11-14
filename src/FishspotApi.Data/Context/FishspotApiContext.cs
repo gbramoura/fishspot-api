0e0baf1fef01816db7e21f80a0af99b5c8b6bdf3
Gabriel Moura <gabrielalves.dev@gmail.com>
2024-11-14 18:37:37 -0300

    add mongodb context to the project

---
+++ src/FishspotApi.Data/Context/FishspotApiContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishspotApi.Data.Context
+{
+    public class FishspotApiContext
+    {
+        private readonly IMongoDatabase? _database;
+
+        public FishspotApiContext(string dbConnection)
+        {
+            var mongoUrl = MongoUrl.Create(dbConnection);
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        public IMongoDatabase? Database => _database;
+    }
+}
